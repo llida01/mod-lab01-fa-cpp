3a7d90165e266adacd16e230e0a8ad12044fdc9e
llida01 <126851615+llida01@users.noreply.github.com>
2023-03-10 13:20:52 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,12 @@
 #include <iostream>
 #include <string>
 #include "fun.h"
-using namespace std;
 
 int main() {
     char str[1000];
-    cin.getline(str, 1000);
-    cout << "Words without numbers: " << faStr1(str) << endl;
-    cout << "Words with a capital: " << faStr2(str) << endl;
-    cout << "Average word length: " << faStr3(str) << endl;
+    std::cin.getline(str, 1000);
+    std::cout << "Words without numbers: " << faStr1(str) << endl;
+    std::cout << "Words with a capital: " << faStr2(str) << endl;
+    std::cout << "Average word length: " << faStr3(str) << endl;
     return 0;
 }
